96e58adedfa53dfcc7430bf777668f58f98988ad
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2017-02-05 11:37:02 +0100

    Production Run

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ from waitress import serve
 
 # Run Application
 if __name__ == "__main__":
-    app = create_app('development')
+    app = create_app('production')
     with app.app_context():
         mg.User().register('dirk', 'olse')
-    if platform.node() == "CAA2GKCOR1":
+    if platform.node() == "CAA2GKCOR1XXX":
         app.run(host="0.0.0.0", port=5012, debug=True)
     else:
-        port = int(os.environ.get("PORT", 5012))
+        port = int(os.environ.get("PORT", 80))
         serve(app, port=port)
